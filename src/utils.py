7115a44490c2e1724b18ae011be19a81a41cd05d
KinaneH <habra.kinane@gmail.com>
2024-12-08 17:29:29 +0100

    cleaned comments and useless file

--- src/utils.py
+++ src/utils.py
@@ -88,7 +88,7 @@ def get_model(model_name):
 def evaluate_model(model, X_val, y_val, metric='f1'):
     y_val_pred = model.predict(X_val)
     if metric == 'f1':
-        return f1_score(y_val, y_val_pred, average='binary')  # Change 'binary' to 'macro', 'micro', or 'weighted' for multi-class
+        return f1_score(y_val, y_val_pred, average='binary')
     elif metric == 'accuracy':
         return accuracy_score(y_val, y_val_pred)
     else:
