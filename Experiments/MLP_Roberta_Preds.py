856b1aaff8b39f7e3b40755dfa3d593d12108005
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2024-12-18 19:29:21 +0100

    Update MLP_Roberta_Preds.py

--- Experiments/MLP_Roberta_Preds.py
+++ Experiments/MLP_Roberta_Preds.py
@@ -10,29 +10,6 @@ from Models.MLPwithText import CustomMLP
 from src.helpers.data_loader import create_dataloader, TextDataset
 
 
-
-def create_csv_submission(ids, y_pred, name):
-    """
-    This function creates a csv file named 'name' in the format required for a submission in Kaggle or AIcrowd.
-    The file will contain two columns the first with 'ids' and the second with 'y_pred'.
-    y_pred must be a list or np.array of 1 and -1 otherwise the function will raise a ValueError.
-
-    Args:
-        ids (list,np.array): indices
-        y_pred (list,np.array): predictions on data correspondent to indices
-        name (str): name of the file to be created
-    """
-    # Check that y_pred only contains -1 and 1
-    if not all(i in [-1, 1] for i in y_pred):
-        raise ValueError("y_pred can only contain values -1, 1")
-
-    with open(name, "w", newline="") as csvfile:
-        fieldnames = ["Id", "Prediction"]
-        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
-        writer.writeheader()
-        for r1, r2 in zip(ids, y_pred):
-            writer.writerow({"Id": int(r1), "Prediction": int(r2)})
-
 if __name__ == "__main__":
 
     # Define paths and model names
