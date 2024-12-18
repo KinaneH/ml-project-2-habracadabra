69d1567c51ac481a761d02a02360692e36b71d2e
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2024-12-18 23:57:13 +0100

    Update MLPwithText.py

--- Models/MLPwithText.py
+++ Models/MLPwithText.py
@@ -247,7 +247,7 @@ def evaluate_model(dataloader, pretrained_model, mlp, device, use_cls='cls'):
     print(f"Evaluation completed in {str(timedelta(seconds=int(total_time)))}")
     print(f"Accuracy: {accuracy:.4f}, F1 Score: {f1:.4f}")
 
-    return accuracy
+    return accuracy, f1
 
 
 
