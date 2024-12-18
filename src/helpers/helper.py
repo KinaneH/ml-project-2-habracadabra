90535ee55bf7a45211416ffa66e3ce9af1c9667c
sofiya-hub <69193875+sofiya-hub@users.noreply.github.com>
2024-12-18 19:29:55 +0100

    Update helper.py

--- src/helpers/helper.py
+++ src/helpers/helper.py
@@ -82,3 +82,25 @@ def load_data(path_train_pos, path_train_neg):
     print(f"Number of negative tweets loaded: {len(neg_train)}")
 
     return pos_train, neg_train
+
+def create_csv_submission(ids, y_pred, name):
+    """
+    This function creates a csv file named 'name' in the format required for a submission in Kaggle or AIcrowd.
+    The file will contain two columns the first with 'ids' and the second with 'y_pred'.
+    y_pred must be a list or np.array of 1 and -1 otherwise the function will raise a ValueError.
+
+    Args:
+        ids (list,np.array): indices
+        y_pred (list,np.array): predictions on data correspondent to indices
+        name (str): name of the file to be created
+    """
+    # Check that y_pred only contains -1 and 1
+    if not all(i in [-1, 1] for i in y_pred):
+        raise ValueError("y_pred can only contain values -1, 1")
+
+    with open(name, "w", newline="") as csvfile:
+        fieldnames = ["Id", "Prediction"]
+        writer = csv.DictWriter(csvfile, delimiter=",", fieldnames=fieldnames)
+        writer.writeheader()
+        for r1, r2 in zip(ids, y_pred):
+            writer.writerow({"Id": int(r1), "Prediction": int(r2)})
